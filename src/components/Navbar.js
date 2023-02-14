7e5c049de7bd43912062bcc99378c1dcaa4f1461
Infant Vishal <infant.vishal@indusnet.co.in>
2023-02-14 15:57:58 +0530

    added tooltip on hover for fav page icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { AiOutlineHeart } from "react-icons/ai";
 import "../assets/css/navbar.css";
 
 const NavbarComponent = () => {
@@ -13,11 +14,22 @@ const NavbarComponent = () => {
         <Navbar.Brand className="navbar-brand-name">Hello, Vishal</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          {location.pathname == "/memes" ? (
+          {location.pathname === "/memes" ? (
             <Link to="/fav_memes" className="me-4">
-              <AiOutlineHeart color="white" fontSize="2em" />
+              <OverlayTrigger
+                placement="bottom"
+                overlay={
+                  <Tooltip id="info-icon-tooltip">
+                    One click away from your favorite meme page!
+                  </Tooltip>
+                }
+              >
+                <div>
+                  <AiOutlineHeart color="white" fontSize="2em" />
+                </div>
+              </OverlayTrigger>
             </Link>
-          ) : location.pathname == "/fav_memes" ? (
+          ) : location.pathname === "/fav_memes" ? (
             <Link to="/memes">
               <button className="btn btn-light me-4">All memes</button>
             </Link>
